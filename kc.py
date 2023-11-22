10d856379c045d6ccaa9e520b57926602a4474aa
mouse040429 <mouse040429@126.com>
2023-11-22 22:57:55 +0800

    更正历史价格排序

--- kc.py
+++ kc.py
@@ -3,6 +3,7 @@
 import re
 import json
 import time
+import datetime
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,75 +17,78 @@ def checkThreadIds():
         'https://keylol.com/forum.php?mod=forumdisplay&fid=234&filter=author&orderby=dateline&typeid=913').text
     threads = BeautifulSoup(res, 'html.parser').select(
         '#threadlisttableid > tbody')
-    for i in range(10):
+    for i in range(12):
         result = re.search(r'(\d+)', threads[i].attrs['id'])
         if result:
             tid = result.group(1)
             if not tid in last_tids:
-                getThreadContent(tid)
                 tids.append(tid)
+    for tid in reversed(tids):
+        items = getThreadContent(tid)
+        updateData(items)
     tids.extend(last_tids)
-    update = {'date': int(time.time()), 'tids': tids[:10]}
+    update = {'date': int(time.time()), 'tids': tids[:12]}
     open('F:/keylol-chinaplay-table/update.json', 'w',
          encoding='utf-8').write(json.dumps(update, ensure_ascii=False))
 
 
 def getThreadContent(tid):
     global date
+    items = []
+
+    def readNode(node):
+        global discount
+        for _node in node.contents:
+            if isinstance(_node, str):
+                match = re.search(r'(\d+(\.\d+)?)元', _node)
+                if match:
+                    discount = float(match.group(1))
+            elif _node.name == 'a':
+                cp_href = re.match(
+                    r'https:\/\/chinaplay\.store\/detail\/(\S+)\/', _node.attrs['href'])
+                if cp_href:
+                    print(cp_href.group(1))
+                    items.append([cp_href.group(1), [discount, date]])
+            else:
+                readNode(_node)
     res = requests.get('https://keylol.com/t'+tid+'-1-1').text
     soup = BeautifulSoup(res, 'html.parser')
-    date = re.search(r'\d+-\d+-\d+', soup.select_one('.plc .authi em').text) or re.search(
-        r'\d+-\d+-\d+', soup.select_one('.plc .authi em span').attrs['title']).group(0)
-    floor1 = soup.select_one('.t_fsz > table .t_f')
-    readNode(floor1)
+    if soup.select_one('.plc .authi em'):
+        date = (re.search(r'\d+-\d+-\d+', soup.select_one('.plc .authi em').text) or re.search(
+            r'\d+-\d+-\d+', soup.select_one('.plc .authi em span').attrs['title'])).group(0)
+        floor1 = soup.select_one('.t_fsz > table .t_f')
+        readNode(floor1)
+    return items
 
 
-def readNode(node):
-    global discount
-    global date
-    global items
-    if isinstance(node, str):
-        match = re.search(r'(\d+(\.\d+)?)元', node)
-        if match:
-            discount = float(match.group(1))
-    else:
-        if node.name == 'a':
-            cp_href = re.match(
-                r'https:\/\/chinaplay\.store\/detail\/(\S+)\/', node.attrs['href'])
-            if cp_href:
-                items.append([cp_href.group(1), [discount, date]])
-        else:
-            for _node in node.contents:
-                readNode(_node)
-
-
-def updateData():
-    global items
+def updateData(items):
     data = json.loads(
         open('F:/keylol-chinaplay-table/data.json', 'r', encoding='utf-8').read())
-    for item in items:
-        if item[0] in data:
-            hist = [item[1]]
-            hist.extend(data[item[0]]['hist'])
-            hist = hist[:10]
-            if item[1][0] <= data[item[0]]['low'][0]:
-                data[item[0]] = {'low': item[1], 'hist': hist}
+    for kv in items:
+        if kv[0] in data:
+            new_hist = kv[1]
+            if new_hist[0] <= data[kv[0]]['low'][0]:
+                data[kv[0]]['low'] = new_hist
+            _hist = data[kv[0]]['hist']
+            if (datetime.datetime.strptime(new_hist[1], '%Y-%m-%d').date() - datetime.datetime.strptime(_hist[-1][1], '%Y-%m-%d').date()).days < 16:
+                if _hist[0][0] > new_hist[0]:
+                    _hist[0] = new_hist
             else:
-                data[item[0]]['hist'] = hist
+                arr = [new_hist]
+                arr.extend(_hist)
+                data[kv[0]]['hist'] = arr[:10]
         else:
-            data[item[0]] = {'low': item[1], 'hist': [item[1]]}
-    open('data.json', 'w',
+            data[kv[0]] = {'low': kv[1], 'hist': [kv[1]]}
+    open('F:/keylol-chinaplay-table/data.json', 'w',
          encoding='utf-8').write(json.dumps(data, ensure_ascii=False, separators=(',', ':')))
 
 
 def main():
     checkThreadIds()
-    updateData()
 
 
 date = None
 discount = 0
-items = []
 
 if __name__ == '__main__':
     main()
